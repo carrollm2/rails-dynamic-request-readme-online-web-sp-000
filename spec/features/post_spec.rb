64ecb06f8a297eac4661b6172526d9b298a8a5f3
Michael Carroll <carrollm2@gmail.com>
2020-05-17 17:25:12 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,7 +13,7 @@ describe 'navigate' do
   it 'shows the title on the show page in an h1 tag' do
     visit "/posts/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
-  end  
+  end
 
   it 'shows the description on the show page in a p tag' do
     visit "/posts/#{@post.id}"
